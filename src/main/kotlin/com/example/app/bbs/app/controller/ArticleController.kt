d91d1a6ed50485d2bed7c80cd1cdef0aa306384b
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-23 11:07:38 +0900

    registerArticle関数のリファクタリング

--- src/main/kotlin/com/example/app/bbs/app/controller/ArticleController.kt
+++ src/main/kotlin/com/example/app/bbs/app/controller/ArticleController.kt
@@ -1,9 +1,11 @@
 package com.example.app.bbs.app.controller
 
+import com.example.app.bbs.app.request.ArticleRequest
 import com.example.app.bbs.domain.entity.Article
 import com.example.app.bbs.domain.repository.ArticleRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
@@ -16,22 +18,16 @@ class ArticleController {
 
     @PostMapping("/")
     @ResponseBody
-    fun registerArticle(
-        @RequestParam name: String,
-        @RequestParam title: String,
-        @RequestParam contents: String,
-        @RequestParam articleKey: String
-    ): String {
+    fun registerArticle(@ModelAttribute articleRequest: ArticleRequest): String {
         articleRepository.save(
             Article(
-                0,
-                name,
-                title,
-                contents,
-                articleKey
+                articleRequest.id,
+                articleRequest.name,
+                articleRequest.title,
+                articleRequest.contents,
+                articleRequest.articleKey
             )
         )
         return "Saved"
     }
-
 }
