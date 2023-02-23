f30eef6911ddcb91309d434e184f3e04cded85c1
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-23 10:09:32 +0900

    Articleテーブルを操作するインターフェースの実装、controllerの実装

--- src/main/kotlin/com/example/app/bbs/domain/repository/ArticleRepository.kt
+++ src/main/kotlin/com/example/app/bbs/domain/repository/ArticleRepository.kt
@@ -1,2 +1,7 @@
 package com.example.app.bbs.domain.repository
 
+import com.example.app.bbs.domain.entity.Article
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ArticleRepository : JpaRepository<Article, Int>
+
