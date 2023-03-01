cb0f5be7311fa21da713d2fb86dd6ddf2d28b52c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-01 10:48:46 +0900

    更新機能の実装

--- src/main/kotlin/com/example/app/bbs/app/controller/ArticleController.kt
+++ src/main/kotlin/com/example/app/bbs/app/controller/ArticleController.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @Controller
 class ArticleController {
@@ -34,5 +35,38 @@ class ArticleController {
         return "index"
     }
 
+    @GetMapping("/edit/{id}")
+    fun getArticleEdit(@PathVariable id: Int, model: Model): String {
+
+        return if (articleRepository.existsById(id)) {
+            model.addAttribute("article", articleRepository.findById(id))
+            "edit"
+        } else {
+            "redirect:/"
+        }
+    }
+
+    @PostMapping("/update")
+    fun updateArticle(articleRequest: ArticleRequest): String {
+        if (!articleRepository.existsById(articleRequest.id)) {
+            return "redirect:/"
+        }
+
+        val article: Article = articleRepository.findById(articleRequest.id).get()
+
+        if (articleRequest.articleKey != article.articleKey) {
+            return "redirect:/edit/${articleRequest.id}"
+        }
+
+        article.name = articleRequest.name
+        article.title = articleRequest.title
+        article.contents = articleRequest.contents
+        article.updateAt = Date()
+
+        articleRepository.save(article)
+
+        return "redirect:/"
+
+    }
 
 }
--- src/test/kotlin/com/example/app/bbs/unit/controller/ArticleControllerTests.kt
+++ src/test/kotlin/com/example/app/bbs/unit/controller/ArticleControllerTests.kt
@@ -1,11 +1,13 @@
 package com.example.app.bbs.unit.controller
 
 import com.example.app.bbs.app.controller.ArticleController
+import com.example.app.bbs.domain.entity.Article
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -47,6 +49,78 @@ class ArticleControllerTests {
             .andExpect(status().isOk)
             .andExpect(model().attributeExists("articles"))
             .andExpect(view().name("index"))
+    }
+
+    @Test
+    fun getArticleEditNotExistsIdTest() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/edit/" + 0)
+        )
+            .andExpect(status().is3xxRedirection)
+            .andExpect(view().name("redirect:/"))
+    }
+
+    @Test
+    @Sql(statements = ["INSERT INTO article (name, title, contents, article_key) VALUES ('test', 'test', 'test', 'test');"])
+    fun getArticleEditExistsIdTest() {
+
+        val latestArticle: Article = target.articleRepository.findAll().last()
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/edit/" + latestArticle.id)
+        )
+            .andExpect(status().isOk)
+            .andExpect(view().name("edit"))
+    }
+
+
+    @Test
+    fun updateArticleNotExistsArticleTest() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/update")
+                .param("id", "0")
+                .param("name", "test")
+                .param("title", "test")
+                .param("contents", "test")
+                .param("articleKey", "err.")
+        )
+            .andExpect(status().is3xxRedirection)
+            .andExpect(view().name("redirect:/"))
+    }
+
+    @Test
+    @Sql(statements = ["INSERT INTO article (name, title, contents, article_key,register_at, update_at) VALUES ('test', 'test', 'test', 'test', now(), now());"])
+    fun updateArticleNotMatchArticleKeyTest() {
+
+        val latestArticle: Article = target.articleRepository.findAll().last()
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/update")
+                .param("id", latestArticle.id.toString())
+                .param("name", latestArticle.name)
+                .param("title", latestArticle.title)
+                .param("contents", latestArticle.contents)
+                .param("articleKey", "err.")
+        )
+            .andExpect(status().is3xxRedirection)
+            .andExpect(view().name("redirect:/edit/${latestArticle.id.toString()}"))
+    }
+
+    @Test
+    @Sql(statements = ["INSERT INTO article (name, title, contents, article_key, register_at, update_at) VALUES ('test', 'test', 'test', 'test', now(), now());"])
+    fun updateArticleExistsArticleTest() {
+
+        val latestArticle: Article = target.articleRepository.findAll().last()
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/update")
+                .param("id", latestArticle.id.toString())
+                .param("name", latestArticle.name)
+                .param("title", latestArticle.title)
+                .param("contents", latestArticle.contents)
+                .param("articleKey", latestArticle.articleKey)
+        )
+            .andExpect(status().is3xxRedirection)
+            .andExpect(view().name("redirect:/"))
     }
 
 }
